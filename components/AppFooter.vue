b7a3fff9c5ea7b27db482f8420e097a48da2ac92
Alek <alekissac@gmail.com>
2020-09-26 21:14:49 -0500

    initial commit

---
+++ components/AppFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer>Copyright info goes here !</footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ components/AppMenu.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav>
+        <ul>
+            <li><nuxt-link to="/">Home</nuxt-link></li>
+            <nuxt-link to="/restaurants">Restaurants</nuxt-link>
+        </ul>
+    </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
